0fdc29f1ab4add26b87c8827e970afea8787c8e6
Ayo <aolag00@gmail.com>
2021-03-15 03:06:27 +0000

    Addedd Home compnents, tweaked more CSS

---
+++ src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../../App.css'
+// hero section here if we want one
+
+function Home() {
+    return (
+        <>
+            <h1>I guess this is my website...</h1>
+        </>
+    )
+}
+
+export default Home
